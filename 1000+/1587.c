731c447e65024ed7e1007f8bc20a0e67b1829410
icys13 <jiajun.li13@gmail.com>
2013-11-17 21:58:07 +0800

    AC 1587

---
+++ 1000+/1587.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m, price, min;
+    while (scanf("%d %d", &n, &m) != EOF)
+    {
+        min = 10000000;
+        while (n--)
+        {
+            scanf("%d", &price);
+            if (price < min)
+                min = price;
+        }
+        printf("%d\n", m/min);
+    }
+    return 0;
+}

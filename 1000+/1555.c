bd062059449d6f3bdb30a91e01280a3eac9c05b8
icys13 <jiajun.li13@gmail.com>
2013-11-17 14:04:10 +0800

    1555

---
+++ 1000+/1555.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, k, sum;
+    while (scanf("%d %d", &m, &k), m || k)
+    {
+        sum = 0;
+        while (m >= k)
+        {
+            m -= k;
+            sum += k;
+            m ++;
+        }
+        sum += m;
+        printf("%d\n", sum);
+    }
+    return 0;
+}

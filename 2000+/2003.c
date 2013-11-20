9dc4ab778e68e03376a302bab815587c9fba0482
icys13 <jiajun.li13@gmail.com>
2013-11-20 17:07:57 +0800

    AC 2003

---
+++ 2000+/2003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define abs(x) (x < 0)? -x: x
+
+int main()
+{
+    double c;
+    while (scanf("%lf", &c) != EOF)
+    {
+        printf("%.2lf\n", abs(c));
+    }
+    return 0;
+}

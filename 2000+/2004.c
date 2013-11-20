aa3c8491a6d4e0419b5e4e11acf5457a777d6f11
icys13 <jiajun.li13@gmail.com>
2013-11-20 17:14:33 +0800

    AC 2004

---
+++ 2000+/2004.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    while (scanf("%d", &score) != EOF)
+    {
+        if (score < 0 || score > 100)
+            printf("Score is error!\n" );
+        else if (score >= 90 && score <= 100)
+            printf("A\n");
+        else if (score >= 80 && score <= 89)
+            printf("B\n");
+        else if (score >= 70 && score <= 79)
+            printf("C\n");
+        else if (score >= 60 && score <= 69)
+            printf("D\n");
+        else
+            printf("E\n");
+    }
+    return 0;
+}

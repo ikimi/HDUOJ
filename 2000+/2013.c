4ed4c4ffc599b5d06e71fd95824d8a12e58c41a6
icys13 <jiajun.li13@gmail.com>
2013-11-21 13:01:04 +0800

    AC 2013

---
+++ 2000+/2013.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int data[35];
+void init (void);
+
+int main()
+{
+    int n;
+    init ();
+    while (scanf ("%d", &n) != EOF)
+    {
+        printf("%d\n", data[n]);
+    }
+    return 0;
+}
+
+void init (void)
+{
+    int i;
+    data[1] = 1;
+    for (i = 2; i < 35; i++)
+        data[i] = 2 * (data[i - 1] + 1);
+}

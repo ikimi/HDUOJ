9438e6ed673c29fa48c223e6fc27a4b39db719d7
icys13 <jiajun.li13@gmail.com>
2013-11-22 20:19:19 +0800

    AC 2021

---
+++ 2000+/2021.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int money[6] = {100, 50, 10, 5, 2, 1};
+int sol (int x);
+
+int main()
+{
+    int n, sum, x;
+    while (scanf("%d", &n), n)
+    {
+        sum = 0;
+        while (n--)
+        {
+            scanf ("%d", &x);
+            sum += sol (x);
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}
+
+int sol (int x)
+{
+    int i, sum;
+    sum = 0;
+    for (i = 0; i < 6;)
+        if (x >= money[i])
+        {
+            sum += 1;
+            x -= money[i];
+        }
+        else
+            i++;
+    return sum;
+}

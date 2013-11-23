97ec2db72739189c60c847e3ff6073b35b60a162
icys13 <jiajun.li13@gmail.com>
2013-11-23 11:12:36 +0800

    AC 2025

---
+++ 2000+/2025.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX 110
+
+char data[MAX];
+
+int main()
+{
+    char max;
+    int i;
+    while (scanf("%s", data) != EOF)
+    {
+        max = '0';
+        for (i = 0; data[i] != '\0'; i++)
+        {
+            if (data[i] > max)
+                max = data[i];
+        }
+        for (i = 0; data[i] != '\0'; i++)
+        {
+            putchar(data[i]);
+            if (data[i] == max)
+                    printf("(max)");
+        }
+        printf ("\n");
+    }
+    return 0;
+}

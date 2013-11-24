b94a0bc68e0f7d021f8df9e9cdedc1e1558a926c
icys13 <jiajun.li13@gmail.com>
2013-11-24 16:37:12 +0800

    AC 2040

---
+++ 2000+/2040.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int solve (int x);
+int main()
+{
+    int M, A, B;
+    scanf ("%d", &M);
+    while (M--)
+    {
+        scanf ("%d%d", &A, &B);
+        if (solve(A) == B && A == solve(B))
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
+
+int solve (int x)
+{
+    int i, sum;
+    for (i = 1, sum = 0; i < x; i++)
+        if (!(x % i))
+            sum += i;
+    return sum;
+}

c9402227146faaa72c7c90b9bd73d07b1c5003cf
icys13 <jiajun.li13@gmail.com>
2013-11-24 15:54:11 +0800

    AC 2039

---
+++ 2000+/2039.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define MAX(x, y) x > y ? x : y
+
+int main()
+{
+    int n;
+    double a, b, c, sum;
+    scanf ("%d", &n);
+    while (n--)
+    {
+        scanf ("%lf%lf%lf", &a, &b, &c);
+        sum = a + b + c;
+        c = MAX(MAX(a, b), c);
+        if (sum - c > c)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}

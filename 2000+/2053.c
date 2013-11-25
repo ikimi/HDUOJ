91c5356ccb1625d96527b43db98a814a1890af0e
icys13 <jiajun.li13@gmail.com>
2013-11-25 20:49:07 +0800

    AC 2053

---
+++ 2000+/2053.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, flag;
+    while (scanf ("%d", &n) != EOF)
+    {
+        i = 1; flag = -1;
+        while (i <= n)
+            if (!(n % i++))
+                flag *= -1;
+        if (flag < 0)
+            printf("0\n");
+        else
+            printf("1\n");
+    }
+    return 0;
+}

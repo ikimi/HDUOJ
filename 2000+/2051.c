7b6c04f3a341df3a16adbb3899348079015056c6
icys13 <jiajun.li13@gmail.com>
2013-11-25 20:30:44 +0800

    AC 2051

---
+++ 2000+/2051.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void solve (int x);
+int main()
+{
+    int n;
+    while (scanf ("%d", &n) != EOF)
+    {
+        solve(n);
+        printf("\n");
+    }
+    return 0;
+}
+
+void solve (int x)
+{
+    if (!x)
+        return ;
+    solve(x / 2);
+    printf("%d", x % 2);
+}

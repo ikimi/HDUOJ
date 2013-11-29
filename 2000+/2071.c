b682e3922792767ec996929d611bb34768ffa4ee
icys13 <jiajun.li13@gmail.com>
2013-11-29 20:25:35 +0800

    AC 2071

---
+++ 2000+/2071.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, n;
+    double max, temp;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        max = -1;
+        while(n--)
+        {
+            scanf("%lf", &temp);
+            if (temp > max)
+                max = temp;
+        }
+        printf("%.2lf\n", max);
+    }
+    return 0;
+}

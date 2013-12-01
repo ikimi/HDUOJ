902602fb934cb48c9489ac2828f56b9815f0a25f
icys13 <jiajun.li13@gmail.com>
2013-12-01 16:50:11 +0800

    AC 2090

---
+++ 2000+/2090.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[100];
+    double n, price, sum = 0;
+    int temp;
+
+    while (scanf("%s%lf%lf", name, &n, &price) != EOF)
+    {
+        sum += n * price;
+    }
+    sum *= 100;
+    temp = (int)sum;
+    if (temp % 10 > 4)
+        sum = temp / 10 + 1;
+    else
+        sum = temp / 10;
+    printf("%.1lf\n", sum / 10);
+    return 0;
+}

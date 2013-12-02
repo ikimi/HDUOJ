6f139309b90ea96e86ce4423a30d908d3d383198
icys13 <jiajun.li13@gmail.com>
2013-12-02 17:02:15 +0800

    AC 2099

---
+++ 2000+/2098.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <memory.h>
+
+int data[10001];
+void init();
+
+int main()
+{
+    int n, num, i;
+
+    init();
+    while (scanf("%d", &n), n)
+    {
+        num = 0;
+        for (i = 2; i < n / 2; i++)
+        {
+            if (data[i] && data[n - i])
+                num++;
+        }
+        printf("%d\n", num);
+    }
+    return 0;
+}
+
+void init()
+{
+    int i, j;
+
+    memset(data, 1, sizeof(data));
+    for (i = 2; i <= 10000; i++)
+    {
+        if (data[i])
+        {
+            for (j = 2; i * j <= 10000; j++)
+                if (data[i * j])
+                    data[i * j] = 0;
+        }
+    }
+}

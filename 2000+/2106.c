c8a5c6af846d30b7ab07a42bd80dc66ef4ae8af4
icys13 <jiajun.li13@gmail.com>
2013-12-02 19:42:27 +0800

    AC 2106

---
+++ 2000+/2106.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int sol(int x, int lap);
+int main()
+{
+    int n, sum, lap, x;
+
+    while (scanf("%d", &n) != EOF)
+    {
+        sum = 0;
+        while (n--)
+        {
+            scanf("%d(%d)", &x, &lap);
+            sum += sol(x, lap);
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}
+
+int sol(int x, int lap)
+{
+    int i, sum;
+
+    i = sum = 0;
+    if (lap == 10)
+        return x;
+    while (x)
+    {
+        sum += (x % 10) * pow(lap, i++);
+        x /= 10;
+    }
+    return sum;
+}

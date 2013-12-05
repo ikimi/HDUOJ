b567c005304ecc110916c1c4c6d081c53ee03269
icys13 <jiajun.li13@gmail.com>
2013-12-05 18:30:49 +0800

    AC 2139

---
+++ 2000+/2139.c
@@ -0,0 +1,17 @@
+/*
+    超时算法
+*/
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum;
+
+    while (scanf("%d", &n) != EOF)
+    {
+        for (i = 1, sum = 0; i <= n; i += 2)
+            sum += i * i;
+        printf("%d\n", sum);
+    }
+    return 0;
+}

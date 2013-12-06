1b9f02affc0f8c19cf8b7eba5374d466f9393f0c
icys13 <jiajun.li13@gmail.com>
2013-12-06 15:14:58 +0800

    AC 2162

---
+++ 2000+/2162.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum, n, x, count = 0;
+
+    while (scanf("%d", &n), n > 0)
+    {
+        sum = 0;
+        while (n--)
+        {
+            scanf("%d", &x);
+            sum += x;
+        }
+        printf("Sum of #%d is %d\n", ++count, sum);
+    }
+    return 0;
+}

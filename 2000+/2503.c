c6165dbb495e34cd83f1f368127c5e1631a0ffea
icys13 <jiajun.li13@gmail.com>
2013-12-07 19:51:10 +0800

    AC 2503

---
+++ 2000+/2503.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int gcd(int a, int b);
+
+int main()
+{
+    int N, a, b, c, d, n;
+
+    scanf("%d", &N);
+    while (N--)
+    {
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        n = gcd(a * d + b * c, b * d);
+        printf("%d %d\n", (a * d + b * c) / n, (b * d) / n);
+    }
+    return 0;
+}
+
+int gcd(int a, int b)
+{
+    int temp;
+
+    if (a < b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    while (b)
+    {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+
+    return a;
+}

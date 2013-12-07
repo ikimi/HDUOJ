e28830c62ccdaaf89ef1b102661d7845f4563fb0
icys13 <jiajun.li13@gmail.com>
2013-12-07 19:19:15 +0800

    AC 2500

---
+++ 2000+/2500.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, x, i, j;
+
+    scanf("%d", &N);
+    while (N--)
+    {
+        scanf ("%d", &x);
+        for (i = 1; i <= 3 * x; i++)
+        {
+            for (j = 1; j <= x; j++)
+                printf("HDU");
+            printf("\n");
+        }
+    }
+    return 0;
+}

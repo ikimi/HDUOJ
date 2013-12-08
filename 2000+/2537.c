fa1c0412f546a17c3cb6aaf6914b537296d22650
icys13 <jiajun.li13@gmail.com>
2013-12-08 17:12:57 +0800

    AC 2537

---
+++ 2000+/2537.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, Y, R;
+    char c;
+
+    while (scanf("%d", &N), N)
+    {
+        Y = R = 0;
+        getchar();
+        while (--N)
+        {
+            c = getchar();
+            if (c == 'R')
+                R++;
+            if (c == 'Y')
+                Y++;
+        }
+        if ((c = getchar()) == 'B')
+        {
+            if (R < 7)
+                printf("Yellow\n");
+            else
+                printf("Red\n");
+        }
+        else
+        {
+            if (Y < 7)
+                printf("Red\n");
+            else
+                printf("Yellow\n");
+        }
+    }
+    return 0;
+}

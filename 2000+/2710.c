79b665d514f0301592d172e3c4f7c9b4c1680f69
icys13 <jiajun.li13@gmail.com>
2013-12-09 20:36:39 +0800

    AC 2710

---
+++ 2000+/2710.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int data[20010];
+void init(void);
+
+int main()
+{
+    int N, x, max;
+
+    init();
+    while (scanf("%d", &N) != EOF)
+    {
+        max = 1;
+        while (N--)
+        {
+            scanf("%d", &x);
+            if (data[x] > data[max])
+                max = x;
+        }
+        printf("%d\n", max);
+    }
+    return 0;
+}
+
+void init(void)
+{
+    int i, j;
+
+    for (i = 1; i < 20010; i++)
+        data[i] = 1;
+    for (i = 2; i < 20010; i++)
+        if (data[i] == 1)
+            for (j = 1; i * j < 20010; j++)
+                data[i * j] = i;
+}

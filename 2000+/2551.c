84939bbe42988b410970242d6f6e955aa888acfb
icys13 <jiajun.li13@gmail.com>
2013-12-09 16:22:36 +0800

    AC 2551

---
+++ 2000+/2551.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int t, n, i;
+
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        i = 1;
+        while (n > pow(i * (i + 1)/2, 2))
+            i++;
+        printf("%d\n", i);
+    }
+    return 0;
+}

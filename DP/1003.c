86c94f701cff26144666ba1f553ee9b7f4fff9dc
icys13 <jiajun.li13@gmail.com>
2013-12-12 20:46:07 +0800

    AC DP 1003

---
+++ DP/1003.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+struct node
+{
+    int start;
+    int sum;
+} data[100010];
+
+int main()
+{
+    int T, x, n, i, j, max;
+
+    scanf("%d", &T);
+    for (j = 1; j <= T; j++)
+    {
+        scanf("%d", &n);
+        scanf("%d", &data[0].sum);
+        data[0].start = 0;
+        for (i = 1; i < n; i++)
+        {
+            scanf("%d", &x);
+            if (data[i - 1].sum + x >= x)
+            {
+                data[i].sum = data[i - 1].sum + x;
+                data[i].start = data[i - 1].start;
+            }
+            else
+            {
+                data[i].sum = x;
+                data[i].start = i;
+            }
+        }
+        for (i = max = 0; i < n; i++)
+            if (data[max].sum < data[i].sum)
+                max = i;
+        printf("Case %d:\n", j);
+        printf("%d %d %d\n", data[max].sum, data[max].start + 1, max + 1);
+        if (j % T)
+            printf("\n");
+    }
+    return 0;
+}

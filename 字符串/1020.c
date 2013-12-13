3fe9a8f5c827a8592fac933d5089ef923ccef76a
icys13 <jiajun.li13@gmail.com>
2013-12-13 20:46:11 +0800

    AC 1020

---
+++ 字符串/1020.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+char data[1010];
+int main()
+{
+    int T, i, j, len;
+
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%s", data);
+        len = strlen(data);
+        for (i = 0; i < len;)
+        {
+            for (j = i + 1; data[i] == data[j] && j < len; j++);
+            if (j >= len)
+            {
+                if (j - i > 1)
+                    printf("%d%c", j - i, data[i]);
+                else
+                    printf("%c", data[i]);
+                printf("\n");
+                break;
+            }
+            else
+            {
+                if (j - i > 1)
+                    printf("%d%c", j - i, data[i]);
+                else
+                    printf("%c", data[i]);
+                i = j;
+            }
+        }
+    }
+    return 0;
+}

0f397ded28e327fe3d4e95c993a0b6dc0596ba3c
icys13 <jiajun.li13@gmail.com>
2013-12-15 15:55:09 +0800

    AC 1161

---
+++ 字符串/1161.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define tolower(x) x >= 'A' && x <= 'Z' ? x + 32 : x
+
+int main()
+{
+    char str[1010];
+    int i;
+
+    while (gets(str))
+    {
+        i = 0;
+        while (str[i])
+        {
+            putchar(tolower(str[i]));
+            i++;
+        }
+        putchar('\n');
+    }
+    return 0;
+}
